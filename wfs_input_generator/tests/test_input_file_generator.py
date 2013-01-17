bfa3268bca983f3113b115d21618f71408776db4
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-01-17 14:20:11 +0100

    Added event reading test case.

--- wfs_input_generator/tests/test_input_file_generator.py
+++ wfs_input_generator/tests/test_input_file_generator.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-DESCRIPTION
+Test suite for the waveform solver input file generator.
 
 :copyright:
     Lion Krischer (krischer@geophysik.uni-muenchen.de), 2013
 :license:
-    GNU Lesser General Public License, Version 3
-    (http://www.gnu.org/copyleft/lesser.html)
+    GNU General Public License, Version 3
+    (http://www.gnu.org/copyleft/gpl.html)
 """
 from wfs_input_generator import InputFileGenerator
 
 import inspect
+from obspy.core import UTCDateTime
 import os
 import unittest
 
@@ -35,6 +36,8 @@ class InputFileGeneratorTestCase(unittest.TestCase):
         gen = InputFileGenerator()
         gen.add_stations([seed_file_1, seed_file_2])
 
+        # Sort to be able to compare.
+        stations = sorted(gen._stations)
         self.assertEqual([
             {"id": "BW.FURT",
              "latitude": 48.162899,
@@ -45,7 +48,43 @@ class InputFileGeneratorTestCase(unittest.TestCase):
              "latitude": 47.737167,
              "longitude": 12.795714,
              "elevation_in_m": 860.0,
-             "local_depth_in_m": 0.0}], gen._stations)
+             "local_depth_in_m": 0.0}], stations)
+
+    def test_readingQuakeMLFiles(self):
+        """
+        Tests the reading of QuakeML Files.
+        """
+        event_file_1 = os.path.join(self.data_dir, "event1.xml")
+        event_file_2 = os.path.join(self.data_dir, "event2.xml")
+
+        gen = InputFileGenerator()
+        gen.add_events([event_file_1, event_file_2])
+
+        # Sort to be able to compare.
+        events = sorted(gen._events)
+        self.assertEqual([{
+            "latitude": 28.7,
+            "longitude": -113.1,
+            "depth_in_km": 13.0,
+            "origin_time": UTCDateTime(2012, 4, 12, 7, 15, 48, 500000),
+            "m_rr": -2.11e+18,
+            "m_tt": -4.22e+19,
+            "m_pp": 4.43e+19,
+            "m_rt": -9.35e+18,
+            "m_rp": -8.38e+18,
+            "m_tp": -6.44e+18
+        }, {
+            "latitude": 13.93,
+            "longitude": -92.47,
+            "depth_in_km": 28.7,
+            "origin_time": UTCDateTime(2012, 11, 7, 16, 35, 55, 200000),
+            "m_rr": 1.02e+20,
+            "m_tt": -7.96e+19,
+            "m_pp": -2.19e+19,
+            "m_rt": 6.94e+19,
+            "m_rp": -4.08e+19,
+            "m_tp": 4.09e+19
+            }], events)
 
 
 def suite():
