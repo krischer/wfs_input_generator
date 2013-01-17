ee16ec41aba941f6a37e17d5a4bfb9c27db516de
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-01-17 13:50:46 +0100

    Setup test structure.

--- wfs_input_generator/tests/test_input_file_generator.py
+++ wfs_input_generator/tests/test_input_file_generator.py
@@ -9,11 +9,26 @@ DESCRIPTION
     GNU Lesser General Public License, Version 3
     (http://www.gnu.org/copyleft/lesser.html)
 """
+from wfs_input_generator import InputFileGenerator
 
+import inspect
+import os
 import unittest
+
 
 class InputFileGeneratorTestCase(unittest.TestCase):
     """
     Test case for the general InputFileGenerator.
     """
-    def setUp(self
+    def setUp(self):
+        # Most generic way to get the actual data directory.
+        self.data_dir = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), "data")
+
+
+def suite():
+    return unittest.makeSuite(InputFileGeneratorTestCase, "test")
+
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="suite")
