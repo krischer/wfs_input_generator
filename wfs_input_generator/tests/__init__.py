1f0330ad72fb36837a7838970ab6b41066b18b21
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2013-03-26 09:49:34 +0100

    Fully automatic test suite discovery.

--- wfs_input_generator/tests/__init__.py
+++ wfs_input_generator/tests/__init__.py
@@ -1,19 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Test module file first running flake8 on all files and then running all tests.
+
+:copyright:
+    Lion Krischer (krischer@geophysik.uni-muenchen.de), 2013
+:license:
+    GNU General Public License, Version 3
+    (http://www.gnu.org/copyleft/gpl.html)
+"""
 # -*- coding: utf-8 -*-
 import flake8.main
+import glob
 import inspect
 import os
 import unittest
-
-# Import the necessary files and append them to a list. These modules will be
-# searched for unittests.
-import test_input_file_generator
-modules = (test_input_file_generator, )
+import warnings
 
 
 def suite():
     """
     Automatic unittest discovery.
+
+    Will discover all test suites in files called test*.py in the tests folder.
     """
+    # Get a list of all files.
+    files = glob.glob(os.path.join(os.path.dirname(os.path.abspath(
+        inspect.getfile(inspect.currentframe()))), "test*.py"))
+    files = [os.path.splitext(os.path.basename(_i))[0] for _i in files]
+
+    modules = []
+    # try to import all files.
+    for module in files:
+        try:
+            module = __import__(module, globals(), locals())
+        except:
+            warnings.warn("Module %s could not be imported" % module)
+            continue
+        modules.append(module)
+
     suite = unittest.TestSuite()
     for module in modules:
         for attrib in dir(module):
